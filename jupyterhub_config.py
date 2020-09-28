117ec6092f1c584990c4339f02768e9e1db9d4cd
Toni Gurgui <toni.gurgui@gmail.com>
2020-09-28 07:24:44 +0000

    Updated jupyterhub configuration

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -10,6 +10,50 @@ c = get_config()
 # avoid having to rebuild the JupyterHub container every time we change a
 # configuration parameter.
 
+## Generate certs used for internal ssl
+#  Default: False
+c.JupyterHub.generate_certs = False
+
+# User containers will access hub by container name on the Docker network
+## The ip address for the Hub process to *bind* to.
+#  
+#  By default, the hub listens on localhost only. This address must be accessible
+#  from the proxy and user servers. You may need to set this to a public ip or ''
+#  for all interfaces if the proxy or user servers are in containers or on a
+#  different host.
+#  
+#  See `hub_connect_ip` for cases where the bind and connect address should
+#  differ, or `hub_bind_url` for setting the full bind URL.
+#  Default: '127.0.0.1'
+c.JupyterHub.hub_ip = 'jupyterhub'
+
+## The internal port for the Hub process.
+#  
+#  This is the internal port of the hub itself. It should never be accessed
+#  directly. See JupyterHub.port for the public port to use when accessing
+#  jupyterhub. It is rare that this port should be set except in cases of port
+#  conflict.
+#  
+#  See also `hub_ip` for the ip and `hub_bind_url` for setting the full bind URL.
+#  Default: 8081
+c.JupyterHub.hub_port = 8080
+
+# TLS config
+
+## The public facing port of the proxy.
+#  
+#  This is the port on which the proxy will listen. This is the only port through
+#  which JupyterHub should be accessed by users.
+#  
+#  .. deprecated: 0.9
+#      Use JupyterHub.bind_url
+#  Default: 8000
+c.JupyterHub.port = 80
+#c.JupyterHub.ssl_key = os.environ['SSL_KEY']
+#c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
+
+
+
 # Spawn single-user servers as Docker containers
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 # Spawn containers from this image
@@ -43,16 +87,41 @@ c.DockerSpawner.remove_containers = True
 # For debugging arguments passed to spawned containers
 c.DockerSpawner.debug = True
 
-# User containers will access hub by container name on the Docker network
-c.JupyterHub.hub_ip = 'jupyterhub'
-c.JupyterHub.hub_port = 8080
+## Maximum number of cpu-cores a single-user notebook server is allowed to use.
+#  
+#  If this value is set to 0.5, allows use of 50% of one CPU. If this value is
+#  set to 2, allows use of up to 2 CPUs.
+#  
+#  The single-user notebook server will never be scheduled by the kernel to use
+#  more cpu-cores than this. There is no guarantee that it can access this many
+#  cpu-cores.
+#  
+#  **This is a configuration setting. Your spawner must implement support for the
+#  limit to work.** The default spawner, `LocalProcessSpawner`, does **not**
+#  implement this support. A custom spawner **must** add support for this setting
+#  for it to be enforced.
+#  Default: None
+c.DockerSpawner.cpu_limit = 1
+## Maximum number of bytes a single-user notebook server is allowed to use.
+#  
+#  Allows the following suffixes:
+#    - K -> Kilobytes
+#    - M -> Megabytes
+#    - G -> Gigabytes
+#    - T -> Terabytes
+#  
+#  If the single user server tries to allocate more memory than this, it will
+#  fail. There is no guarantee that the single-user notebook server will be able
+#  to allocate this much memory - only that it can not allocate more than this.
+#  
+#  **This is a configuration setting. Your spawner must implement support for the
+#  limit to work.** The default spawner, `LocalProcessSpawner`, does **not**
+#  implement this support. A custom spawner **must** add support for this setting
+#  for it to be enforced.
+#  Default: None
+c.Spawner.mem_limit = '2G'
 
-# TLS config
-c.JupyterHub.port = 80
-#c.JupyterHub.ssl_key = os.environ['SSL_KEY']
-#c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
-
-# Authenticate users with GitHub OAuth
+# Authenticate users with Dummy Authenticator
 c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
 #c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
 
